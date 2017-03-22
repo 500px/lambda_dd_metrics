c88f2f90c708d5ea57563de4d261400b57f72a4d
Vladimir Li <vlad@500px.com>
2017-03-22 12:10:31 -0400

    added basic structure and copied

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='lamdba_dd_metrics',
+      version='0.1',
+      description='Wrapper around reporting metrics to DataDog from AWS Lambda python functions',
+      url='http://github.com/500px/lamdba_dd_metrics',
+      author='Vladimir Li',
+      author_email='vlad@500px.com',
+      license='Apache',
+      packages=['lamdba_dd_metrics'],
+      zip_safe=False)
