82217b397697f233304e5232c90a84291c40e8c5
Vladimir Li <vlad@500px.com>
2017-03-23 11:12:30 -0400

    update the package to build correctly

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
 setup(name='lambda_dd_metrics',
-      version='0.1',
+      version='0.3',
       description='Wrapper around reporting metrics to DataDog from AWS Lambda python functions',
       url='http://github.com/500px/lambda_dd_metrics',
       author='Vladimir Li',
       author_email='vlad@500px.com',
       license='Apache',
+      py_modules=['lambda_dd_metrics'],
       zip_safe=True)
