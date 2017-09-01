38388b1edb55be04680458420a977de0589b826c
Matt Stobo <mstobo@500px.com>
2017-09-01 10:39:59 -0400

    Add histogram support

--- lambda_dd_metrics.py
+++ lambda_dd_metrics.py
@@ -6,15 +6,16 @@ Simple interface for reporting metrics to DataDog.
 from __future__ import print_function
 import time
 
+
 class DataDogMetrics(object):
     '''
-    Datadog supports printing to stdout to report metrics.
-    This only gives us counts and gauges:
+    Datadog supports printing to stdout to report metrics. This only gives us
+    counts and gauges:
         https://www.datadoghq.com/blog/monitoring-lambda-functions-datadog
 
-    Another method would be via the API but that one only supports gauges
-        and requires auth, which I'd rather not do until they've added
-        support for histograms and counts.
+    Another method would be via the API but that one only supports gauges and
+    requires auth, which I'd rather not do until they've added support for
+    histograms and counts.
     '''
     def __init__(self, service_prefix, stats_group):
         self.service_prefix = service_prefix
@@ -22,7 +23,8 @@ class DataDogMetrics(object):
 
     def incr(self, metric_name, count=1, tags=None):
         '''
-        Incr - Increment a counter metric, providing a count of occurances per second
+        Incr - Increment a counter metric, providing a count of occurrences per
+        second.
         '''
         full_metric_name = self._build_metric_name(metric_name)
         all_tags = self._build_tags(tags)
@@ -30,9 +32,10 @@ class DataDogMetrics(object):
 
     def gauge(self, metric_name, value, tags=None):
         '''
-        Gauge - Gauges are a constant data type. They are not subject to averaging,
-        and they don't change unless you change them. That is, once you set a gauge value,
-        it will be a flat line on the graph until you change it again
+        Gauge - Gauges are a constant data type. They are not subject to
+        averaging, and they don't change unless you change them. That is, once
+        you set a gauge value, it will be a flat line on the graph until you
+        change it again.
         '''
         full_metric_name = self._build_metric_name(metric_name)
         all_tags = self._build_tags(tags)
@@ -42,7 +45,25 @@ class DataDogMetrics(object):
         '''
         Histogram - Send a histogram, tracking multiple samples of a metric
         '''
-        # NOT SUPPORTED YET
+        full_metric_name = self._build_metric_name(metric_name)
+        all_tags = self._build_tags(tags)
+        self._print_metric('histogram', full_metric_name, value, all_tags)
+
+    def timer(self, metric_name, tags=None):
+        '''
+        Timer - A convenient decorator that automatically records the runtime
+        of your function and reports it as a histogram.
+        '''
+        def decorator(function):
+            def wrapper(*args, **kwargs):
+                start_time = time.time()
+                ret_val = function(*args, **kwargs)
+                duration = time.time() - start_time
+                self.histogram(metric_name, duration, tags)
+                return ret_val
+
+            return wrapper
+        return decorator
 
     def timing(self, metric_name, delta, tags=None):
         '''
@@ -64,6 +85,8 @@ class DataDogMetrics(object):
 
     def _print_metric(self, metric_type, metric_name, value, tags):
         unix_epoch_timestamp = int(time.time())
-        print('MONITORING|{0}|{1}|{2}|{3}|#{4}'.format(
-            unix_epoch_timestamp, value, metric_type, metric_name, ','.join(tags)
-        ))
+        print('MONITORING|{0}|{1}|{2}|{3}|#{4}'.format(unix_epoch_timestamp,
+                                                       value,
+                                                       metric_type,
+                                                       metric_name,
+                                                       ','.join(tags)))
