0d5920c7bb0bab8364b86ca3ce9c0babad070207
Matt Stobo <mstobo@500px.com>
2017-09-05 11:09:53 -0400

    Change setup.py to use Platform Team

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='lambda_dd_metrics',
-      version='0.4',
+      version='1.0',
       description='Wrapper around reporting metrics to DataDog from AWS Lambda python functions',
       url='http://github.com/500px/lambda_dd_metrics',
-      author='Vladimir Li',
-      author_email='vlad@500px.com',
+      author='500px Platform Team',
+      author_email='platform@500px.com',
       license='Apache',
       py_modules=['lambda_dd_metrics'],
       zip_safe=True)
